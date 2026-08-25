43676fe80efcaa74c1a1d4c3fa449644aad8f36a
hey24blanket <hey24blanket@gmail.com>
2026-08-25 14:29:21 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+// Firebase 프로젝트 설정 (한 번만 입력해 두면 index.html에서 자동으로 불러옵니다)
+window.firebaseConfig = {
+  apiKey: "AIzaSyDhN4RjhOrqYAs3OdJdAfEQqlO60O5qXlQ",
+  authDomain: "saju-grap.firebaseapp.com",
+  projectId: "saju-grap",
+  storageBucket: "saju-grap.firebasestorage.app",
+  messagingSenderId: "498768758041",
+  appId: "1:498768758041:web:aaa2f9ff0b4ffba26ef188",
+  measurementId: "G-FD2WYJZYC1"
+};
